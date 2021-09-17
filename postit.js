a531fbdb3dcf6e5b4d102cfdf6cf5f1fa2151bcd
Robuchon Hugues <robuchon.hugues@orange.fr>
2021-09-17 23:37:06 +0200

    mise en fonction des boutons V et S

--- postit.js
+++ postit.js
@@ -19,14 +19,22 @@ rdv.addEventListener("click", function() {
     btn_v.classList.add("btn_v");
     btn_v.innerText = "Validation";
     btn_v.addEventListener("click", function() {
+        /* evenement validation recup du textarea a rentré dans un "p" a créer */
         console.log("valid");
+        let p = document.createElement("p");
+        let saisie = document.querySelector("textarea");
+        p.innerText = saisie.value;
+        saisie.remove();
+        card.insertBefore(p, card.children[1]);
     });
     /* création du BTN suppretion et l'evenement */
     let btn_s = document.createElement("p");
     btn_s.classList.add("btn_s");
     btn_s.innerText = "Suppression";
     btn_s.addEventListener("click", function() {
+        /* evenement suppression */
         console.log("suppr");
+        card.remove();
     });
     /* fourrage des div */
     card.appendChild(btn_v);
