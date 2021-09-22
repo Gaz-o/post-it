b2ccbcc9e8432afc4a419f24c3b1740aa1e57460
Robuchon Hugues <robuchon.hugues@orange.fr>
2021-09-22 09:31:38 +0200

    reglage de bug

--- postit.js
+++ postit.js
@@ -5,9 +5,10 @@ let todo = document.querySelector(".p-3");
 let wlist = document.querySelector(".p-4");
 
 let tableau = new Array
-if (JSON.parse(localStorage.getItem("BDD")) != null) {
-    tableau = JSON.parse(localStorage.getItem("BDD"))
+if (JSON.parse(localStorage.getItem("BDDdeTEST")) != null) {
+    tableau = JSON.parse(localStorage.getItem("BDDdeTEST"))
 };
+
 let save_data = {
     class: "",
     data: "textarea",
@@ -16,62 +17,67 @@ let save_data = {
 }
 
 for (let li in tableau) {
-    console.log("chargement de la "+li+"\n",tableau[li]);
-    let save = Object.create(save_data)
-    save = tableau[li]
+    if (tableau[li] == null) {
+        console.log("Bug de ", li);
+    } else {
 
-    let card = document.createElement("card");
-    card.classList.add(save.class);
-    let btn_v = document.createElement("p");
-    btn_v.classList.add("btn_v");
-    btn_v.innerText = "Validation";
-    let p = document.createElement("p");
-    p.classList.add("info")
-    p.innerText = save.data
-    let btn_s = document.createElement("p");
-    btn_s.classList.add("btn_s");
-    btn_s.innerText = "Suppression";
-
-    let d
-    if (save.data_sup != undefined) {
-        d = document.createElement("p");
-        d.classList.add("date")
-        d.innerText = save.data_sup
-    }
-    let couleur = save.class
-    let coul = save.class_2
-
-    btn_v.addEventListener("click", function () {
-        card.classList.toggle(coul);
-        card.classList.toggle(couleur);
-        save.class = card.classList.value;
-        if (save.class == coul) {
-            save.class_2 = couleur
-        } else {
-            save.class_2 = coul
+        console.log("chargement de la "+li+"\n",tableau[li]);
+        let save = Object.create(save_data)
+        save = tableau[li]
+    
+        let card = document.createElement("card");
+        card.classList.add(save.class);
+        let btn_v = document.createElement("p");
+        btn_v.classList.add("btn_v");
+        btn_v.innerText = "Validation";
+        let p = document.createElement("p");
+        p.classList.add("info")
+        p.innerText = save.data
+        let btn_s = document.createElement("p");
+        btn_s.classList.add("btn_s");
+        btn_s.innerText = "Suppression";
+    
+        let d
+        if (save.data_sup != undefined) {
+            d = document.createElement("p");
+            d.classList.add("date")
+            d.innerText = save.data_sup
         }
-        tableau[li] = save
-        console.log("changement de couleur", save)
-        localStorage.clear()
-        localStorage.setItem("BDD", JSON.stringify(tableau));
-    });
-
-    btn_s.addEventListener("click", function () {
-        tableau.splice(li, 1);
-        localStorage.clear()
-        localStorage.setItem("BDD", JSON.stringify(tableau));
-        console.log("suppr", li);
-        card.remove();
-        window.location.reload()
-    });
-    card.appendChild(btn_v);
-    if (tableau[li].data_sup != undefined) {
-        card.appendChild(d);
+        let couleur = save.class
+        let coul = save.class_2
+    
+        btn_v.addEventListener("click", function () {
+            card.classList.toggle(coul);
+            card.classList.toggle(couleur);
+            save.class = card.classList.value;
+            if (save.class == coul) {
+                save.class_2 = couleur
+            } else {
+                save.class_2 = coul
+            }
+            tableau[li] = save
+            console.log("changement de couleur", save)
+            localStorage.clear()
+            localStorage.setItem("BDDdeTEST", JSON.stringify(tableau));
+        });
+    
+        btn_s.addEventListener("click", function () {
+            tableau.splice(li, 1);
+            localStorage.removeItem("BDDdeTEST")
+            localStorage.setItem("BDDdeTEST", JSON.stringify(tableau));
+            console.log("suppr", li);
+            card.remove();
+            window.location.reload()
+        });
+        card.appendChild(btn_v);
+        if (tableau[li].data_sup != undefined) {
+            card.appendChild(d);
+        }
+        card.appendChild(p);
+        card.appendChild(btn_s);
+        let affichage = document.querySelector(".tableau");
+        affichage.appendChild(card);
     }
-    card.appendChild(p);
-    card.appendChild(btn_s);
-    let affichage = document.querySelector(".tableau");
-    affichage.appendChild(card);
 }
 
 evenement(rdv, "vert", "date")
@@ -149,19 +155,21 @@ function evenement(btn, couleur, scdbtn) {
             }
             tableau[key] = save
             console.log("Save Ok", key);
-            localStorage.clear()
-            localStorage.setItem("BDD", JSON.stringify(tableau));
+            localStorage.removeItem("BDDdeTEST")
+            localStorage.setItem("BDDdeTEST", JSON.stringify(tableau));
+            window.location.reload()
         });
         card.appendChild(btn_v);
         if (scdbtn == "date") {
             card.appendChild(sup);
         }
         btn_s.addEventListener("click", function () {
-            delete tableau[key]
+            tableau.splice(key, 1)
             card.remove();
             console.log("suppr", key);
-            localStorage.clear()
-            localStorage.setItem("BDD", JSON.stringify(tableau));
+            localStorage.removeItem("BDDdeTEST")
+            localStorage.setItem("BDDdeTEST", JSON.stringify(tableau));
+            window.location.reload()
         });
         card.appendChild(text);
         card.appendChild(btn_s);
